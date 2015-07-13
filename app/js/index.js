76978bf6c28591b700405f5d837eac99f01d944b
jacobdlewis <jacobdlewis@gmail.com>
2015-07-13 00:34:25 -0500

    updated to 3-day forecast

--- app/js/index.js
+++ app/js/index.js
@@ -1,36 +1,39 @@
 $.getJSON('http://api.wunderground.com/api/543f71dec17544bc/forecast10day/q/32344.json', function(data) {
   response = data.forecast.simpleforecast.forecastday;
+  response = response.slice(0,3);
   console.log(response);
   $forecast = $('#five-day-forecast');
   response.forEach(function(day) {
-    $forecast.append("<div class='card'>" +
-                        "<div class='card-content'>" +
-                          "<div class='card-title red-text center-align'>" +
-                            day.date.weekday +
-                            ", " +
-                            day.date.monthname_short +
-                            " " +
-                            day.date.day +
+    $forecast.append("<div class='col l4 s12'>" +
+                        "<div class='card'>" +
+                          "<div class='card-content'>" +
+                            "<div class='card-title red-text center-align'>" +
+                              day.date.weekday +
+                              ", " +
+                              day.date.monthname_short +
+                              " " +
+                              day.date.day +
+                            "</div>" +
+                            "<div class='center-align'>" +
+                              "<img src=" +
+                                day.icon_url +
+                              ">" +
+                            "</div>" +
+                            "<table class='striped centered'>" +
+                              "<tr>" +
+                                "<th>Hi:</th>" +
+                                  "<td>" + day.high.fahrenheit + "</td>" +
+                              "</tr>" +
+                              "<tr>" +
+                                "<th>Lo:</th>" +
+                                  "<td>" + day.low.fahrenheit + "</td>" +
+                              "</tr>" +
+                              "<tr>" +
+                                "<th>Humidity:</th>" +
+                                  "<td>" + day.avehumidity + "</td>" +
+                              "</tr>" +
+                            "</table>" +
                           "</div>" +
-                          "<div class='center-align'>" +
-                            "<img src=" +
-                              day.icon_url +
-                            ">" +
-                          "</div>" +
-                          "<table class='striped centered'>" +
-                            "<tr>" +
-                              "<th>Hi:</th>" +
-                                "<td>" + day.high.fahrenheit + "</td>" +
-                            "</tr>" +
-                            "<tr>" +
-                              "<th>Lo:</th>" +
-                                "<td>" + day.low.fahrenheit + "</td>" +
-                            "</tr>" +
-                            "<tr>" +
-                              "<th>Humidity:</th>" +
-                                "<td>" + day.avehumidity + "</td>" +
-                            "</tr>" +
-                          "</table>" +
                         "</div>" +
                       "</div>");
   });
